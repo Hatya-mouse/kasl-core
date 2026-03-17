f4132a8c9091ef2366dcc272f3328412ee74901c
Hatya-mouse <shuntar30@gmail.com>
2026-03-17 21:37:19 +0900

    Comment out jump instruction

--- src/backend/func_translator/expr_translators/func_call_translator.rs
+++ src/backend/func_translator/expr_translators/func_call_translator.rs
@@ -57,10 +57,10 @@ impl FuncTranslator<'_> {
         }
 
         // Translate the block
-        let has_return = self.translate_block(block, func_return_block);
-        if !has_return {
-            self.builder.ins().jump(func_return_block, &[]);
-        }
+        self.translate_block(block, func_return_block);
+        // if !has_return {
+        //     self.builder.ins().jump(func_return_block, &[]);
+        // }
 
         // Add some arguments to the return block
         self.builder.switch_to_block(func_return_block);
